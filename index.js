121bc960a93800fc4a409825d64f5f1eb3ace6d0
Bleak Chandler <bleakchandler@gmail.com>
2021-03-21 17:37:34 -0400

    commit tree 4ac7926a989a2a71250fd92e00cc8a2b9301782c

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(name, email) {
+
+    // Deliverable 1
+
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+
+      // Deliverable 2
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+
+      // Deliverable 3
+
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+
+}
+
+
+
